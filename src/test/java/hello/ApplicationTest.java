8244479dcda6b5f90645bb1210219ee0b292cf57
Cyrille Volclair <cyrille.volclair@gmail.com>
2018-07-22 11:36:41 -0400

    reaching 100% coverage test

--- src/test/java/hello/ApplicationTest.java
+++ src/test/java/hello/ApplicationTest.java
@@ -1,4 +1,31 @@
 package hello;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(SpringRunner.class)
+@PrepareForTest(SpringApplication.class)
 public class ApplicationTest {
+
+    @Test
+    public void applicationRun() {
+        mockStatic(SpringApplication.class);
+        String[] args = {"toto", "tutu"};
+
+        Application.main(args);
+
+        verifyStatic(SpringApplication.class);
+        SpringApplication.run(Application.class, args);
+    }
+
 }
